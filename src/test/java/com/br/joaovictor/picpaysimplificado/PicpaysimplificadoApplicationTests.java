c9a10bce1eb4aea6a5e148e03c62d79634d99069
Joao Victor <joaovictorcorrealuzia5@proton.me>
2026-07-21 13:36:53 -0300

    Projeto iniciado

---
+++ src/test/java/com/br/joaovictor/picpaysimplificado/PicpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.joaovictor.picpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
