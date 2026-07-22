48b3ca8de4ca3d3cc5d836a7451c632f64c7bd95
Joao Victor <joaovictorcorrealuzia5@proton.me>
2026-07-22 12:49:45 -0300

    Implementado metodo de autorização de transação e execução da transação(savalvando os dados no banco de dados)

--- src/main/java/com/br/joaovictor/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/br/joaovictor/picpaysimplificado/services/TransactionService.java
@@ -1,14 +1,17 @@
 package com.br.joaovictor.picpaysimplificado.services;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.br.joaovictor.picpaysimplificado.dtos.TransactionDTO;
+import com.br.joaovictor.picpaysimplificado.infrastructure.entity.Transaction.Transaction;
 import com.br.joaovictor.picpaysimplificado.infrastructure.entity.Users.User;
 import com.br.joaovictor.picpaysimplificado.repositories.TransactionalRepository;
 
@@ -22,10 +25,41 @@ public class TransactionService {
    @Autowired
    RestTemplate restTemplate;
 
-   public void createTransaction(TransactionDTO transactional) throws Exception{
+   public void createTransaction(TransactionDTO transactional) throws Exception {
       User sender = this.userService.findUserById(transactional.senderId());
       User receiver = this.userService.findUserById(transactional.receiverId());
-   
+
       userService.validatedTransaction(sender, transactional.value());
+
+      boolean isAuthorized = this.authorizeTransaction(sender, transactional.value());
+      if (!isAuthorized) {
+         throw new Exception("Transação não autorizada");
+      }
+
+      Transaction newTransaction = new Transaction();
+      newTransaction.setAmount(transactional.value());
+      newTransaction.setReceiver(receiver);
+      newTransaction.setSender(sender);
+      newTransaction.setTimestamp(LocalDateTime.now());
+
+      sender.setBalance(sender.getBalance().subtract(transactional.value()));
+      receiver.setBalance(receiver.getBalance().add(transactional.value()));
+
+      this.transactionalRepository.save(newTransaction);
+      this.userService.saveUser(sender);
+      this.userService.saveUser(receiver);
+   }
+
+   public boolean authorizeTransaction(User sender, BigDecimal value) {
+      ResponseEntity<Map> authrizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize",
+            Map.class);
+      if (authrizationResponse.getStatusCode() == HttpStatus.OK) {
+         Map dataResponse = (Map) authrizationResponse.getBody().get("data");
+
+         boolean authorization = (boolean) dataResponse.get("authorization");
+         return authorization;
+
+      } else
+         return false;
    }
 }
