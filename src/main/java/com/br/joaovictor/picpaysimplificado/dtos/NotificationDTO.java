c31298c7de4f1326549e2eeb1a8664b160d63bf5
Joao Victor <joaovictorcorrealuzia5@proton.me>
2026-07-22 13:43:29 -0300

    Criação do serviço de notificação, NotificationServices e NotificationDTO

---
+++ src/main/java/com/br/joaovictor/picpaysimplificado/dtos/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.br.joaovictor.picpaysimplificado.dtos;
+
+public record NotificationDTO(String email, String messege) {
+}
---
+++ src/main/java/com/br/joaovictor/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,29 @@
+package com.br.joaovictor.picpaysimplificado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.br.joaovictor.picpaysimplificado.dtos.NotificationDTO;
+import com.br.joaovictor.picpaysimplificado.infrastructure.entity.Users.User;
+
+@Service
+public class NotificationService {
+   @Autowired
+   private RestTemplate restTemplate;
+
+   public void sendNotification(User user, String message) throws Exception {
+      String email = user.getEmail();
+      NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+      ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify",
+            notificationRequest, String.class);
+
+      if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+         System.out.println("Sistema de notificação com erro, erro ao enviar");
+         throw new Exception("Serviço de notificação fora do ar");
+      }
+   }
+}
