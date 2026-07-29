2cf42b7d40d7dc458c0b919d724f4236134e162c
Joao Victor <joaovictorcorrealuzia5@proton.me>
2026-07-29 13:49:14 -0300

    Desenvolvi tests para o metodo findUserById, da classe UserService, test de caso de sucesso e usuario nao encontrado.

--- src/test/java/com/br/joaovictor/picpaysimplificado/services/UserServiceTest.java
+++ src/test/java/com/br/joaovictor/picpaysimplificado/services/UserServiceTest.java
@@ -1,9 +1,13 @@
 package com.br.joaovictor.picpaysimplificado.services;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,8 +29,9 @@ public class UserServiceTest {
    User userTest = new User();
 
    @BeforeEach
-   void setup(){
+   void setup() {
       BigDecimal balance = new BigDecimal(1000);
+      userTest.setId(1L);
       userTest.setBalance(balance);
       userTest.setUserType(UserType.COMMOM);
       userTest.setDocument("123456789");
@@ -35,28 +40,46 @@ public class UserServiceTest {
       userTest.setLastName("De Tests");
       userTest.setPassword("12345678");
    }
-   
-   //-------ValidateTransactionTests--------
+
+   // -------ValidateTransactionTests--------
    @Test
-   void ValidateTransactionSuccessfully(){
+   void ValidateTransactionSuccessfully() {
       BigDecimal amout = new BigDecimal(500);
-      assertDoesNotThrow(() ->
-         userService.validatedTransaction(userTest, amout));
+      assertDoesNotThrow(() -> userService.validatedTransaction(userTest, amout));
    }
+
    @Test
-   void ValidateTransactionWhenBalanceIsInsufficient(){
+   void ValidateTransactionWhenBalanceIsInsufficient() {
       BigDecimal amout = new BigDecimal(1500);
-      Exception exception = 
-         assertThrows(Exception.class,
-             () -> userService.validatedTransaction(userTest, amout));
+      Exception exception = assertThrows(Exception.class,
+            () -> userService.validatedTransaction(userTest, amout));
    }
+
    @Test
-   void ValidateTransactionWhenUserIsMerchant(){
+   void ValidateTransactionWhenUserIsMerchant() {
       BigDecimal amout = new BigDecimal(500);
       userTest.setUserType(UserType.MERCHANT);
-      Exception exception = 
-         assertThrows(Exception.class,
-             () -> userService.validatedTransaction(userTest, amout));
+      Exception exception = assertThrows(Exception.class,
+            () -> userService.validatedTransaction(userTest, amout));
+   }
+   // -------findUserByIdTests--------
+
+   @Test
+   void findUserByIdSuccessfully() throws Exception {
+      when(userRepostory.findUserById(1L))
+         .thenReturn(Optional.of(userTest));
+
+      User user = userService.findUserById(1L);
+      assertEquals(1L, user.getId());
+   }
+
+   @Test
+   void findUserByIdNotFound() {
+      when(userRepostory.findUserById(1L))
+         .thenReturn(Optional.empty());
+
+      assertThrows(Exception.class,
+         () -> userService.findUserById(1L));
    }
 
 }
